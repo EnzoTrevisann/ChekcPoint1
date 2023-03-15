5a8df162b2ce9888a517c75b1b17f06b9f87442e
EnzoTrevisann <enzo.trev@hotmail.com>
2023-03-15 20:45:09 -0300

    -

--- pages/Home/index.jsx
+++ pages/Home/index.jsx
@@ -2,88 +2,83 @@ import React, { useState } from "react";
 import Sons from "../../components/Sons";
 import { Audio } from 'expo-av';
 import { styles } from "./style";
-import {View, Text, TextInput} from "react-native";
+import { View, Text, TextInput } from "react-native";
 import Button from "../../components/Button";
 
-  const sons = [
-     1, 2, 3, 4, 5, 6, 7
-  ]
+const sons = [1, 2, 3, 4, 5, 6, 7];
 
 export default function Home() {
   const [sound, setSound] = React.useState();
   const [handleValue, setHandleValue] = React.useState('');
-  const soundList = []
   const [isPlaying, setIsPlaying] = useState([false, false, false, false, false, false, false]);
+  const [playingIndex, setPlayingIndex] = useState(-1);
+  const soundList = [];
 
+  async function playSound(soundIndex, buttonIndex) {
+    await soundList[soundIndex].playAsync();
 
-  async function playSound(index) {
-    await soundList[index].playAsync();
+    setPlayingIndex(buttonIndex);
+    setIsPlaying(prev => prev.map((_, i) => i === buttonIndex ? true : false)); // atualiza o estado para marcar apenas o botão correspondente
 
-    setIsPlaying((oldList) => {
-      oldList.map((sound,i) =>{
-        console.log(index, i);
-        if(i === index) {
-          return true
-        }
-        return false
-      })
-    })
-    console.log(isPlaying);
-    
     await new Promise((resolve) => {
-      soundList[index].setOnPlaybackStatusUpdate((status) => {
-        if(status.isLoaded && status.didJustFinish){
-          soundList[index].setOnPlaybackStatusUpdate(null) 
+      soundList[soundIndex].setOnPlaybackStatusUpdate((status) => {
+        if (status.isLoaded && status.didJustFinish) {
+          soundList[soundIndex].setOnPlaybackStatusUpdate(null)
           resolve()
-          
         }
       })
-    })
-    setIsPlaying([false, false, false, false, false, false, false]);
-    if(index < soundList.length -1 ) {
-      await playSound(index + 1)
+    });
+
+    setPlayingIndex(-1);
+    setIsPlaying(prev => prev.map((_, i) => false)); // atualiza o estado para desmarcar todos os botões
+
+    if (soundIndex < soundList.length - 1) {
+      await playSound(soundIndex + 1, buttonIndex + 1) // chama a próxima iteração com o próximo som e o próximo botão
     }
   }
 
   React.useEffect(() => {
     return sound
       ? () => {
-          console.log('Unloading Sound');
-          sound.unloadAsync();
-        }
+        console.log('Unloading Sound');
+        sound.unloadAsync();
+      }
       : undefined;
   }, [sound]);
 
   return (
-      <View style={styles.container}>
-        <View style={styles.squareLayout}>{
-        sons.map((__, index)=> (                     
-          <Sons key={index} isPlaying = {false}/>
-        ))                   
-      }</View>
-      
+    <View style={styles.container}>
+      <View style={styles.squareLayout}>
+        {sons.map((value, index) => (
+          <Sons
+            key={index}
+            isPlaying={isPlaying[index]}
+            onPress={() => {
+              const { sound } = Audio.Sound.createAsync(require(`/assets/sounds/audio${value}.mp3`));
+              soundList[index] = sound;
+            }}
+          />
+        ))}
+      </View>
+
       <View style={styles.ViewButton}>
         <Button onPress={async () => {
-          const arrayHandleValue = handleValue.split('')
-          for (let i = 0; i < arrayHandleValue.length; i++){
-            const { sound } = await Audio.Sound.createAsync(require(`/assets/sounds/audio${arrayHandleValue[i]}.mp3`));
-            soundList.push(sound)
-          }
-          await playSound(0)
-          
-        }} >
+          const { sound } = await Audio.Sound.createAsync(require(`/assets/sounds/audio${handleValue}.mp3`));
+          soundList[handleValue - 1] = sound;
+          await playSound(handleValue - 1, handleValue - 1); // passa o índice do som e o índice do botão correspondente
+        }}>
           <Text>Submit</Text>
         </Button>
 
-        
-        <Button onPress={() => setHandleValue('')}> {/* Arrow function que limpa o conteudo dentro do componente button */}
-          <Text>
-            Reset
-          </Text>
+        <Button onPress={(buttonIndex) => {
+          soundList[buttonIndex].unloadAsync();
+          setIsPlaying(prev => prev.map((_, i) => i === buttonIndex ? false : prev[i])); // atualiza o estado para desmarcar apenas o botão pressionado
+        }}>
+          <Text>Reset</Text>
         </Button>
 
         <TextInput style={styles.input} value={handleValue} onChangeText={setHandleValue} />
       </View>
-      </View>
+    </View>
   );
 }
